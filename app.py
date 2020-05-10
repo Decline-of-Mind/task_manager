4873dc5e1f448260131c48147b500d1cec7e942c
Luc Gotink <L.h.gotink@gmail.com>
2020-05-10 09:44:13 +0000

    fixed connection variables

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ def hello():
     return "Hello world... again"
 
 if __name__ == '__main__':
-    app.run(host=environ.get('IP'),
-    port=int(os.environ.get('PORT')),
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
+    port=int(os.environ.get('PORT', '5000')),
     debug=True)
